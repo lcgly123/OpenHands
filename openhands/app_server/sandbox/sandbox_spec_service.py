c6b4ec9f02e2822ca17894d6d6893699db96b25d
Xingyao Wang <xingyaoww@gmail.com>
2026-04-30 19:04:09 -0400

    test: use critic conversation info SDK image

--- openhands/app_server/sandbox/sandbox_spec_service.py
+++ openhands/app_server/sandbox/sandbox_spec_service.py
@@ -13,7 +13,7 @@ from openhands.sdk.utils.models import DiscriminatedUnionMixin
 
 # The version of the agent server to use for deployments.
 # Typically this will be the same as the values from the pyproject.toml
-AGENT_SERVER_IMAGE = 'ghcr.io/openhands/agent-server:3e5a6e6-python'
+AGENT_SERVER_IMAGE = 'ghcr.io/openhands/agent-server:e5bb3b9-python'
 
 
 class SandboxSpecService(ABC):
