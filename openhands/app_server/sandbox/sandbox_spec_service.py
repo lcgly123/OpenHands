87e2f7d54a4ea76a4ec800cb2ac47589aa566fc8
Xingyao Wang <xingyaoww@gmail.com>
2026-05-01 16:56:54 -0400

    test: use latest nullable critic secret image

--- openhands/app_server/sandbox/sandbox_spec_service.py
+++ openhands/app_server/sandbox/sandbox_spec_service.py
@@ -13,7 +13,7 @@ from openhands.sdk.utils.models import DiscriminatedUnionMixin
 
 # The version of the agent server to use for deployments.
 # Typically this will be the same as the values from the pyproject.toml
-AGENT_SERVER_IMAGE = 'ghcr.io/openhands/agent-server:9799cb2-python'
+AGENT_SERVER_IMAGE = 'ghcr.io/openhands/agent-server:9be65c0-python'
 
 
 class SandboxSpecService(ABC):
